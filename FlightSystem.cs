0d0de20a9175d3c232c75ba73317a1c23af1a723
Kelly Condon <krcondon@asu.edu>
2016-01-23 22:42:53 -0700

    Create FlightSystem.cs

---
+++ FlightSystem.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight_Project
+{
+    class FlightSystem
+    {
+        public static Menu flightSystemMenu = new Menu();
+
+        static void Main(string[] args)
+        {
+            flightSystemMenu.MainMenu();
+        }
+    }
+}
